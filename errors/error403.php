b95db6722b2c667a1de2e9a9e0f38cc46aaa242b
Tianle <tianleyin8888@gmail.com>
2024-01-18 19:55:22 +0100

    Errors

---
+++ errors/error403.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 403 - Prohibido</title>
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+</head>
+<body id="error403">
+    <div>
+        <h1>403</h1>
+        <p>No tienes permiso para visualizar esta página</p>
+        <a href="../index.php">Volver a inicio</a>
+    </div>
+</body>
+</html>
---
+++ errors/error404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error 404 - Inexistente</title>
+    <link rel="stylesheet" type="text/css" href="../styles.css">
+</head>
+<body id="error404">
+    <div>
+        <h1>404</h1>
+        <p>La página que estabas buscando no existe</p>
+        <a href="../index.php">Volver a inicio</a>
+    </div>
+</body>
+</html>
