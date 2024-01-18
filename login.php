4861d95f11bf04ef1b2c43721c1710e4d367eced
Tianle <tianleyin8888@gmail.com>
2024-01-18 21:19:30 +0100

    Latest upload 18/01

---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar Sesión</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+    <main id="login">
+        <h1>Iniciar sesión</h1>
+        <form method="post">
+            <input type="email" id="email" placeholder="email" required>
+            <br>
+            <input type="password" id="password" placeholder="contraseña" required>
+            <br>
+            <button type="submit">Iniciar sesión</button>
+        </form>
+    </main>
+</body>
+</html>
