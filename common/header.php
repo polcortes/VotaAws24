43692d6f64a62c0fd1e46bbc6edadfa711d8cb5a
Barney2500 <otaner2002@gmail.com>
2024-01-18 20:59:09 +0100

    [WIP]Includes

---
+++ common/header.php
@@ -0,0 +1,12 @@
+<nav class="navbar">
+<ul>
+    <a href="/index.php"><img src="" srcset=""> ga</a>
+    <li><a href="">Inicio</a></li>
+    <li><a href="">DashBoard</a></li>
+    <li><a href="">Crear Encuesta</a></li>
+</ul>
+<ul>
+    <li><a href="">Iniciar Sesion</a></li>
+    <li><a href="">Registrarse</a></li>
+</ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<?php
+include_once("common/header.php")
+?>
+    <h1>PRUEBA</h1>
+    <?php
+include_once("common/footer.php")
+?>
+</body>
+</html>
