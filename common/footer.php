607f5481d2e87944004970c0ae352109fdfbdad3
Barney2500 <otaner2002@gmail.com>
2024-01-19 18:17:03 +0100

    [WIP] index.html

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,20 @@
 <footer>
-    hola
+    <div class="authors">
+        <div class="continfo">
+            <p class="author">Tianle</p>
+            <p class="mail">algo@algo.com</p>
+        </div>
+        <div class="continfo">
+            <p class="author">Pol</p>
+            <p class="mail">algo@algo.com</p>
+        </div>
+        <div class="continfo">
+            <p class="author">Renato</p>
+            <p class="mail">algo@algo.com</p>
+        </div>
+        <div class="links">
+            <div class="ieti">&copy <a href="https://www.iesesteveterradas.cat/" target="_blank">IES Esteve Terradas i Illa</a></div>
+            <div class="github"><a href="https://github.com/Barney2500/VotaAws24" target="_blank" >GitHub Grupal</a></div>
+        </div>
+    </div>
 </footer>
