dc469ac3847241ac174e00803d7ce1cbd6e23103
Tianle <tianleyin8888@gmail.com>
2024-01-24 16:49:05 +0100

    Fix css CreatePoll

--- create_poll.php
+++ create_poll.php
@@ -1,9 +1,9 @@
 <?php
 session_start();
-// if (!isset($_SESSION['usuario'])) {
-//     header('Location: login.php');
-//     exit();
-// }
+if (!isset($_SESSION['usuario'])) {
+    header('Location: login.php');
+    exit();
+}
 ?> 
 
 <!DOCTYPE html>
