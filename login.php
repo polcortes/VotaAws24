a612612ce1f443febf383440425f0c86a792f2d1
polcortes <pcortesgarcia.cf@iesesteveterradas.cat>
2024-01-24 18:30:44 +0100

    cambio de contraseña de mysql

--- login.php
+++ login.php
@@ -46,7 +46,7 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
 
     // Cambiar parámetros, conexión a BD
     $dsn = "mysql:host=localhost;dbname=votadb";
-    $pdo = new PDO($dsn, 'root', 'p@raMor3'); // AWS24VotaPRRojo_
+    $pdo = new PDO($dsn, 'root', 'AWS24VotaPRRojo_'); // 
 
     // Cambiar query
     $query = $pdo->prepare("SELECT * FROM Users WHERE user_pass = SHA2(:pwd, 512) AND user_mail = :email");
