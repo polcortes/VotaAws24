e115c704dd25ae8d956dd0b8daf9a29ab5779265
Eric Escrich <eric@escrich.net>
2024-02-08 00:06:12 +0100

    myVotes en header y dashboard

--- dashboard.php
+++ dashboard.php
@@ -29,7 +29,7 @@
         <ul>
             <li><a href="list_polls.php">Ver mis encuestas</a></li>
             <li><a href="create_poll.php">Crear encuestas</a></li>
-            <li class="grid-little-item"><a href="#">Ver todas las encuestas</a></li>
+            <li class="grid-little-item"><a href="myVotes.php">Ver mis votos</a></li>
             <li class="grid-little-item"><a href="change_pass.php">Canviar contraseña</a></li>
         </ul>
     </main>
