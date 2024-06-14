d277fce9c303cb0f6f76d657eb468278d119c55b
Pol Cortés <polcortesg@gmail.com>
2024-06-14 23:41:28 +0200

    403 dashboard

--- dashboard.php
+++ dashboard.php
@@ -18,6 +18,7 @@
     <?php
     include_once("common/header.php");
     if (!isset($_SESSION["usuario"])) {
+        include_once("errors/403.php");
         header("HTTP/1.1 403 Forbidden");
         exit();
     }
