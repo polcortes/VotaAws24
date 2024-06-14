28954654c5bf11e7e791dceefd2de841abcf42b7
Pol Cortés <polcortesg@gmail.com>
2024-06-14 23:58:39 +0200

    403 dashboard

--- dashboard.php
+++ dashboard.php
@@ -18,9 +18,9 @@
     <?php
     include_once("common/header.php");
     if (!isset($_SESSION["usuario"])) {
-        include_once("errors/403.php");
-        //header("HTTP/1.1 403 Forbidden");
-        //exit();
+        include_once("errors/error403.php");
+        header("HTTP/1.1 403 Forbidden");
+        exit();
     } else {
     ?>
     <main>
--- prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$usuario = null;
-
-if (isset($usuario)) {
-    echo "Usuario definido";
-} else {
-    include_once("errors/403.php");
-}
